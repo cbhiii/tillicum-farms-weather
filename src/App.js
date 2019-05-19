68e7038f66c9e1ddf1135fb182e9c2ba72421b2e
Chuck Huffman <chuck@huffman.info>
2019-05-19 13:40:21 -0400

    local XML data gets loaded and parsed into state variables.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@
 
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-// import convert from 'xml-js';
+import convert from 'xml-js';
 import './App.css';
 import './assets/weather-icons.css';
-import data from './data.xml';
+import wxData from './data/data.xml';
 import Header from './Header'
 import Current from './Current'
 import Details from './Details'
@@ -16,35 +16,51 @@ import Forecast from './Forecast'
 class App extends Component {
 
   state = {
-    wxData: data
+    actual: [],
+    alltime: [],
+    day1: [],
+    hour1: [],
+    last15m: [],
+    last24h: [],
+    last60m: [],
+    month1: [],
+    year1: []
   }
 
   updateWeather = () => {
+    let result = {};
+    let allData = [];
 
-    // console.log(this.state);
-    // fetch(this.state.data)
-    //   .then((response) => {
-    //     console.log(response);
-    //   })
-    //   .catch((err) => {
-    //       console.log('error:',err);
-    //   })
-    // to convert xml text to json text
-    // let result = convert.xml2json(this.state.data, {compact: false, spaces: 4});
-    // console.log(result);
-
-    // var result2 = convert.xml2json(this.state.data, {compact: false, spaces: 4});
-    // console.log(result2);
-
+    fetch(wxData)
+      .then((response) => response.text())
+       .then(xml => {
+         result = convert.xml2json(xml, {compact: true, spaces: 4});
+         this.setState(state => {
+           allData = JSON.parse(result)
+           state.actual = allData.meteohub.data[0]
+           state.alltime = allData.meteohub.data[1]
+           state.day1 = allData.meteohub.data[2]
+           state.hour1 = allData.meteohub.data[3]
+           state.last15m = allData.meteohub.data[4]
+           state.last24h = allData.meteohub.data[5]
+           state.last60m = allData.meteohub.data[6]
+           state.month1 = allData.meteohub.data[7]
+           state.year1 = allData.meteohub.data[8]
+         }
+         , () => {
+           console.log(this.state.wxData)
+         })
+    })
+    .catch(err => console.log("Error getting and/or processing weather data file: ",err))
   }
 
-  componentDidMount() {
+  componentWillMount() {
+
     this.updateWeather()
+
   }
 
   render() {
-
-    console.log(this.state);
 
     return (
 
